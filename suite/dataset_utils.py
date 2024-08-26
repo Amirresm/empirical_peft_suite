23b1b2968a442ccfed12985f36e3f2f4da0e9e01
Amirreza <amirreza.77.esmaeili@gmail.com>
2024-08-25 18:50:08 -0700

    WIP refact dataset utils

--- suite/dataset_utils.py
+++ suite/dataset_utils.py
@@ -62,3 +62,144 @@ def load_raw_datasets(dataset_name, dataset_config_name, train_file, validation_
 
     return raw_datasets
 
+def get_dataset_mappers(
+        tokenizer,
+        text_column,
+        summary_column,
+        prefix,
+        max_source_length,
+        max_target_length,
+        padding,
+        is_text_tokenized=False,
+        is_summary_tokenized=False,
+        ):
+    def preprocess_encoder_decoder_function(examples):
+        inputs, targets = [], []
+        for i in range(len(examples[text_column])):
+            if examples[text_column][i] and examples[summary_column][i]:
+                input = examples[text_column][i]
+                target = examples[summary_column][i]
+                if data_args.text_tokenized:
+                    input = " ".join(input)
+                if data_args.summary_tokenized:
+                    target = " ".join(target)
+                inputs.append(input)
+                targets.append(target)
+        inputs = [prefix + inp for inp in inputs]
+        # logger.info(f"preprocess inp:\n{inputs[0]}")
+        # logger.info(f"preprocess tgt:\n{targets[0]}")
+        model_inputs = tokenizer(
+            inputs,
+            max_length=data_args.max_source_length,
+            padding=padding,
+            truncation=True,
+        )
+
+        labels = tokenizer(
+            text_target=targets,
+            max_length=max_target_length,
+            padding=padding,
+            truncation=True,
+        )
+
+        # If we are padding here, replace all tokenizer.pad_token_id in the labels by -100 when we want to ignore
+        # padding in the loss.
+        if padding == "max_length" and data_args.ignore_pad_token_for_loss:
+            labels["input_ids"] = [
+                [
+                    (char if char != tokenizer.pad_token_id else -100)
+                    for char in label
+                ]
+                for label in labels["input_ids"]
+            ]
+
+        model_inputs["labels"] = labels["input_ids"]
+        return model_inputs
+
+    def preprocess_decoder_only_function(examples):
+        samples = []
+        for i in range(len(examples[text_column])):
+            if examples[text_column][i] and (
+                summary_column == "NONE" or examples[summary_column][i]
+            ):
+                input = examples[text_column][i]
+                target = (
+                    None
+                    if summary_column == "NONE"
+                    else examples[summary_column][i]
+                )
+                if data_args.text_tokenized:
+                    input = " ".join(input)
+                if target and data_args.summary_tokenized:
+                    target = " ".join(target)
+
+                # input = 'def '.join(input.split('def ')[:2])
+                sample = create_llama_prompt(
+                    input, is_training=True, eos_token=tokenizer.eos_token
+                )
+                samples.append(sample)
+
+        tokenized_samples = tokenizer(
+            samples,
+            max_length=data_args.max_source_length,
+            padding=padding,
+            truncation=True,
+        )
+
+        labels = tokenized_samples["input_ids"]
+        if padding == "max_length" and data_args.ignore_pad_token_for_loss:
+            labels = [
+                [(l if l != tokenizer.pad_token_id else -100) for l in label]
+                for label in labels
+            ]
+
+        tokenized_samples["labels"] = labels
+        return tokenized_samples
+
+    preprocess_function = (
+        preprocess_decoder_only_function
+        if is_decoder_only
+        else preprocess_encoder_decoder_function
+    )
+
+    def generation_preprocess_function(examples):
+        samples = []
+        targets = []
+        for i in range(len(examples[text_column])):
+            if examples[text_column][i]:
+                input = examples[text_column][i]
+                if data_args.text_tokenized:
+                    input = " ".join(input)
+
+                targets.append(
+                    f"{create_llama_prompt(input, is_training=True, eos_token=tokenizer.eos_token)}"
+                )
+                input = '"""'.join(input.split('"""')[:2]) + '"""\n'
+                sample = create_llama_prompt(
+                    input, is_training=False, eos_token=tokenizer.eos_token
+                )
+                samples.append(sample)
+
+        tokenized_samples = tokenizer(
+            samples,
+            max_length=data_args.max_source_length,
+            padding=padding,
+            truncation=True,
+        )
+
+        tokenized_targets = tokenizer(
+            targets,
+            max_length=data_args.max_source_length,
+            padding=padding,
+            truncation=True,
+        )
+
+        labels = tokenized_targets["input_ids"]
+        if padding == "max_length" and data_args.ignore_pad_token_for_loss:
+            labels = [
+                [(l if l != tokenizer.pad_token_id else -100) for l in label]
+                for label in labels
+            ]
+
+        tokenized_samples["labels"] = labels
+        return tokenized_samples
