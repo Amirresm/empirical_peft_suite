7160b9af444893182e08e89956b9e0d4cf27dd05
Amirreza <amirreza.77.esmaeili@gmail.com>
2024-09-03 22:15:58 -0700

    results reader tool

--- tools/present/read_results.py
+++ tools/present/read_results.py
@@ -1,3 +1,4 @@
+import re
 import os
 import re
 import json
@@ -5,8 +6,7 @@ import csv
 from argparse import ArgumentParser
 
 
-datasets = set()
-configs = {}
+
 
 class ConfigMeta:
     def __init__(self, remark, job, model, dataset, peft_lib, peft):
@@ -79,7 +79,9 @@ class Config:
         return f"{self.meta.remark}_{self.meta.job}_{self.meta.model}_{self.meta.dataset}_{self.meta.peft_lib}_{self.meta.peft}: {len(self.all_results)}"
 
 
-def scan_gen(dir: str):
+def scan_dir(dir: str):
+    datasets = set()
+    configs = {}
     # models = [p for p in Path(dir).glob("*.results.json")]
     for model in os.scandir(dir):
         model = model.path
@@ -100,31 +102,119 @@ def scan_gen(dir: str):
                     # print(f"        {config}")
                     configs[config.name] = config
 
+    return configs, datasets
+
+def get_fields(configs):
+    fixed_fields = [
+        "job",
+        "dataset",
+        "remark",
+        "model",
+        "peft_lib",
+        "peft",
+    ]
+    field_groups = [
+        "humaneval",
+        "generate",
+        "predict",
+        "eval",
+        "train",
+    ]
+    field_subgroups = [
+        "BLEU2",
+        "ROUGE",
+        "BLEU",
+        "loss",
+        "pass@",
+        "total_gpu_time",
+        "runtime",
+        "peak_memory",
+        "samples",
+    ]
+    filters = [
+            r"^.*_BLEU.?_(precisions|bleu|brevity_penalty|length_ratio)$",
+            # r"^.*_BLEU.?_((reference|translation)_length)$",
+            # r"^.*ROUGE.*$",
+            r"^.*per_second.*$",
+    ]
+    all_fields = set()
+    for c in configs:
+        all_fields.update(
+            [f for f in c.all_results.keys() if f not in fixed_fields]
+        )
+    main_fields = []
+    all_fields = sorted(all_fields)
+    for fg in field_groups:
+        fg_fields = [f for f in all_fields if f.startswith(fg)]
+        fg_fields_sorted = []
+        for sg in field_subgroups:
+            sg_fields = [f for f in fg_fields if sg in f]
+            for f in sg_fields:
+                if f not in fg_fields_sorted:
+                    fg_fields_sorted.append(f)
+            for f in fg_fields:
+                if f not in fg_fields_sorted:
+                    fg_fields_sorted.append(f)
+
+        for f in fg_fields_sorted:
+            if f not in main_fields:
+                main_fields.append(f)
+
+    for f in all_fields:
+        if f not in main_fields:
+            main_fields.append(f)
+
+    main_fields = [f for f in main_fields if not any([bool(re.search(filter, f)) for filter in filters])]
+
+    all_fields = fixed_fields + main_fields
+
+    return all_fields
+
+def sorted_rows(configs, fields):
+    sort_by = ["remark", "job", "model", "dataset", "peft_lib", "peft"]
+    for by in reversed(sort_by):
+        if by[0] == "-":
+            by = by[1:]
+            configs.sort(key=lambda x: x[by], reverse=True)
+        configs.sort(key=lambda x: x[by])
+
+    configs = list(map(lambda c: {k: c[k] for k in c if k in fields}, configs))
+    return configs
+
 def get_csv(dataset, configs):
     ds_configs = [c for c in configs.values() if c.meta.dataset == dataset]
-    fixed_fields = ["remark", "job", "model", "dataset", "peft_lib", "peft"]
-    all_fields = set()
-    for c in ds_configs:
-        all_fields.update([f for f in c.all_results.keys() if f not in fixed_fields])
-    all_fields = fixed_fields + list(all_fields)
 
-    with open(f"{dataset}.csv", 'w', newline='') as csvfile:
+    all_fields = get_fields(ds_configs)
+
+    rows = sorted_rows([c.all_results for c in ds_configs], all_fields)
+
+    with open(f"{dataset}.csv", "w", newline="") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=all_fields)
         writer.writeheader()
-        writer.writerows([c.all_results for c in ds_configs])
+        writer.writerows(rows)
+
+def run_all(dir, dataset):
+    job_path = "sum" if dataset == "csn-python" else "gen"
+    configs, datasets = scan_dir(os.path.join(dir, job_path))
+
+    get_csv(dataset, configs)
+
 
 def main():
     parser = ArgumentParser()
     parser.add_argument(
-        "--root_dir", type=str, required=True, help="The root directory of the results"
+        "--root_dir",
+        type=str,
+        required=True,
+        help="The root directory of the results",
     )
     args = parser.parse_args()
 
     dir = args.root_dir
 
-    scan_gen(os.path.join(dir, "gen"))
-
-    get_csv("spp_30k", configs)
+    run_all(dir, "spp_30k")
+    run_all(dir, "csn-python")
+    run_all(dir, "sppu_30k")
 
 
 if __name__ == "__main__":
